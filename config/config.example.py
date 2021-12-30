748e2a36390a62170919e431a70115470af9f8f9
matthieugouel <matthieu.gouel@protonmail.com>
2021-12-30 18:29:39 +0100

    refactor exp1

---
+++ config/config.example.py
@@ -0,0 +1,7 @@
+# Iris API credentials
+api_url = "https://api.iris.dioptra.io"
+api_username = ""
+api_password = ""
+
+# Database credentials
+database_url = "https://<USER>:<PASSWORD>@<HOST>?database=<DATABASE>"
